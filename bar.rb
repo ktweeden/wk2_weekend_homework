c23721217272a22194463582b163fd438d52f3f7
Kate Preston <ktweeden@googlemail.com>
2018-02-11 20:18:18 +0000

    refactored check in guests methods

--- bar.rb
+++ bar.rb
@@ -13,35 +13,36 @@ class Bar
   end
 
   def create_rooms(number_of_rooms)
-    (1..number_of_rooms).each() {|number| @rooms << Room.new("Room #{number}")}
+    (1..number_of_rooms).each {|number| @rooms << Room.new("Room #{number}")}
   end
 
   def check_in_guest(guest)
-    available_room = @rooms.find() {|room| room.guests().length < room.capacity}
+    available_room = find_available_room(1)
       if available_room == nil
         @waiting_list << guest
       else
-        available_room.add_guest_to_room(guest) if charge_guest(guest, @entry_fee) != nil
+        add_guest_to_room_if_have_enough_money(guest, available_room)
       end
   end
 
   def check_in_multiple_guests(guest_array)
-    available_room = @rooms.find() do |room|
-      (room.capacity - room.guests().length) >= guest_array.length()
-    end
-
+    available_room = find_available_room(guest_array.length)
     if available_room == nil
-      guest_array.each { |guest| check_in_guest(guest) }
+      guest_array.each {|guest| check_in_guest(guest)}
     else
-      guest_array.each() do |guest|
-        available_room.add_guest_to_room(guest) if charge_guest(guest, @entry_fee) != nil
-      end
+      guest_array.each {|guest| add_guest_to_room_if_have_enough_money(guest, available_room)}
     end
+  end
+
+  def find_available_room(group_size)
+    @rooms.find {|room| (room.capacity - room.guests.length) >= group_size}
   end
 
   def charge_guest(guest, amount)
     guest.pay(amount) if guest.can_afford?(amount)
   end
 
-
+  def add_guest_to_room_if_have_enough_money(guest, room)
+    room.add_guest_to_room(guest) if charge_guest(guest, @entry_fee) != nil
+  end
 end
